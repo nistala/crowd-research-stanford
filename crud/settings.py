155cbe121377300042616cc52080751db9dd3d00
nistala <nrohits@gmail.com>
2015-04-09 00:56:15 -0700

    nice changes

--- crud/settings.py
+++ crud/settings.py
@@ -122,7 +122,7 @@ INSTALLED_APPS = (
     'todo_web',
     'rest_framework',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
--- crud/urls.py
+++ crud/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-
+    url(r'^admin', include(admin.site.urls)),
     url(r'^api', include('todo_api.urls')),
     url(r'^', include('todo_web.urls')),
     # url(r'^', include('todo_web.urls')),
@@ -16,5 +16,4 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
 )
